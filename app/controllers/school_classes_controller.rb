485deda49458e051b060278566a92fe1941e8bfd
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-02-04 17:40:33 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -7,17 +7,20 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
+    @student_class.find(params[:id])
   end
 
   def show
   end
 
   def edit
+    @student_class.find(params[:id])
   end
 
   def update
-    @student.find(params[:id])
-    @student.update(class_params(:title, :room_number))
+    @student_class.find(params[:id])
+    @student_class.update(class_params(:title, :room_number))
+    redirect_to student_class_path(@student_class) 
   end
 
   private
