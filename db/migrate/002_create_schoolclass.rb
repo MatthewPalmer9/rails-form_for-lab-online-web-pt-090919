17a97deb56f1654f2ba94014c72da5f3695f61d9
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-02-04 16:50:31 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_schoolclass.rb
+++ db/migrate/002_create_schoolclass.rb
@@ -2,7 +2,7 @@ class CreateSchoolClass < ActiveRecord::Migration
   def change
     create_table :schoolclasses do |t|
       t.string :title
-      
+      t.integer :room_number 
     end
   end
 end
