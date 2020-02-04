90f12f3f985ad4205cb08db6c807cd0da52db830
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-02-04 17:00:02 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,11 +12,14 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find(params[:id])
   end
 
   def edit
+    @student = Student.find(params[:id])
   end
 
   def update
+    
   end
 end
