cf4385027212a4115221ff0b834c4b6c9af783a1
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-02-04 17:01:54 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -26,6 +26,6 @@ class StudentsController < ApplicationController
   private
 
   def post_params(*args)
-    params.require
+    params.require(:student).permit(:first_name, :last_name))
   end
 end
