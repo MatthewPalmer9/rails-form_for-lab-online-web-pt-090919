dcebe503a7b71b4c44ae3f454814de01ebea58db
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-02-04 17:38:50 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,2 +1,28 @@
 class SchoolClassesController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @student.find(params[:id])
+    @student.update(class_params(:title, ))
+  end
+
+  private
+
+  def class_params(*args)
+    params.require(:student_class).permit(*args)
+  end
 end
